f4386536c21a8cf28d740d39636d8c8a8ce303a0
kyleigma <kyleigma36@gmail.com>
2025-02-14 16:45:56 +0800

    fix sidebar

--- src/includes/sidebar.php
+++ src/includes/sidebar.php
@@ -1,3 +1,9 @@
+<?php include 'includes/header.php'; ?>
+<?php include 'includes/scripts.php'; ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+
+
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
   <ul class="nav">
     <li class="nav-item">
@@ -77,10 +83,5 @@
       </li>
     </ul>
   </nav>
-
-  <!-- Bootstrap JS and dependencies -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
